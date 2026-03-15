21a10ca7e8d3c0d1443e26b816ad3e1f41130b01
GS_anonimato <gsgabrielsousa132@gmail.com>
2026-03-14 22:32:10 -0300

    1.2

--- primeiro.sql
+++ primeiro.sql
@@ -1,11 +1,15 @@
+-- Active: 1769040821303@@localhost@3306@cadastro
 CREATE TABLE pessoas(
-    nome VARCHAR(50),
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(30),
     idade TINYINT,
     sexo CHAR(1),
     peso float,
     altura float,
-    nacionalidade VARCHAR(20)
+    nacionalidade VARCHAR(20),
+    PRIMARY KEY (id)
+)DEFAULT CHARSET=utf8mb4;
 
-);
 
-
+USE cadrastro;
+INSERT INTO pessoas(nome, idade, sexo, peso, altura, nacionalidade) VALUES
