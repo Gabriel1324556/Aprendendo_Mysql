6d1da14d42cf053692dd2a3f709e8fef4b083906
GS_anonimato <gsgabrielsousa132@gmail.com>
2026-03-22 20:54:47 -0300

    1.2.3

--- primeiro.sql
+++ primeiro.sql
@@ -1,15 +1,16 @@
 -- Active: 1769040821303@@localhost@3306@cadastro
-CREATE TABLE pessoas(
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(30),
-    idade TINYINT,
-    sexo CHAR(1),
-    peso float,
-    altura float,
-    nacionalidade VARCHAR(20),
-    PRIMARY KEY (id)
-)DEFAULT CHARSET=utf8mb4;
-
+/*
+?CREATE TABLE pessoas(
+?    id INT NOT NULL AUTO_INCREMENT,
+?    nome VARCHAR(30),
+?    idade TINYINT,
+?    sexo CHAR(1),
+?    peso float,
+?    altura float,
+?    nacionalidade VARCHAR(20),
+?    PRIMARY KEY (id)
+?)DEFAULT CHARSET=utf8mb4;
+*/
 
 /*
 ?USE cadastro;
@@ -25,5 +26,5 @@ CREATE TABLE pessoas(
 /* ou */
 
 USE cadastro;
-INSERT INTO pessoas VALURES
-('Gabriel Sousa Santos', '2009-03-09', 'M', '53', '1.70','Brasil');
+INSERT INTO pessoas VALUES
+(DEFAULT, 'Gabriel Sousa Santos', '2009-03-09', 'M', '53', '1.70','Brasil');
